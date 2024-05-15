d628053ed97c6cfe440b2c8e2eddc88994696b4f
Your Name <you@example.com>
2024-05-15 17:26:02 +0500

    Adding a Simple CLI Calculator Project

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"; 
+
+//printing a Wellcome Message
+console.log("\n\tWellcome To \'alishbahjunaid\' -CLI Simple Calculator\n");
+
+//Asking Question from Users through Inquirer
+
+let answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// coditional statements If-Else
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+} else {
+    console.log("Please select valid operator")
+}
+
+
+
